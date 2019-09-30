cb5f2b23423e67cfe66c794ec3405742e2608c23
sam bossley <35381535+bossley9@users.noreply.github.com>
2019-09-30 15:52:29 -0400

    implemented basic layout and added basic mobile styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
-import './App.css';
+import { FaFacebookF, FaGithub, FaInstagram, FaLinkedinIn, FaSoundcloud } from 'react-icons/fa';
+import { FaIdBadge } from 'react-icons/fa';
+import './App.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <React.Fragment>
+      <div className='container'>
+        <img src={`${process.env.PUBLIC_URL}/profile.jpg`} alt='profile' />
+      </div>
+      <div className='container'>
+        <a href='https://www.facebook.com/bossley9'>
+          <FaFacebookF />
+          <span>facebook.com/bossley9</span>
         </a>
-      </header>
-    </div>
+        <a href='https://www.instagram.com/samaraikun/'>
+          <FaInstagram />
+          <span>instagram.com/samaraikun</span>
+        </a>
+        <a href='https://soundcloud.com/sambossley'>
+          <FaSoundcloud />
+          <span>soundcloud.com/sambossley</span>
+        </a>
+        <a href='https://github.com/bossley9'>
+          <FaGithub />
+          <span>github.com/bossley9</span>
+        </a>
+        <a href='https://www.linkedin.com/in/bossley9/'>
+          <FaLinkedinIn />
+          <span>linkedin.com/in/bossley9</span>
+        </a>
+        {/* TODO favicons */}
+        {/* last updated 09/30/19 */}
+        <a download target='_blank' rel='noopener noreferrer' href={`${process.env.PUBLIC_URL}/Sam_Bossley.pdf`}>
+          <FaIdBadge />
+          <span>resume</span>
+        </a>
+      </div>
+    </React.Fragment>
   );
 }
 
