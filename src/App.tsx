899a0ae503b88313121e731772a9a1cadb5d063b
sam bossley <35381535+bossley9@users.noreply.github.com>
2019-10-01 01:22:43 -0400

    finished mobile layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { FaFacebookF, FaGithub, FaInstagram, FaLinkedinIn, FaSoundcloud } from '
 import { FaIdBadge } from 'react-icons/fa';
 import './App.scss';
 
+// {/* TODO favicons */}
+
 const App: React.FC = () => {
   return (
     <React.Fragment>
@@ -10,32 +12,57 @@ const App: React.FC = () => {
         <img src={`${process.env.PUBLIC_URL}/profile.jpg`} alt='profile' />
       </div>
       <div className='container'>
-        <a href='https://www.facebook.com/bossley9'>
-          <FaFacebookF />
-          <span>facebook.com/bossley9</span>
-        </a>
-        <a href='https://www.instagram.com/samaraikun/'>
-          <FaInstagram />
-          <span>instagram.com/samaraikun</span>
-        </a>
-        <a href='https://soundcloud.com/sambossley'>
-          <FaSoundcloud />
-          <span>soundcloud.com/sambossley</span>
-        </a>
-        <a href='https://github.com/bossley9'>
-          <FaGithub />
-          <span>github.com/bossley9</span>
-        </a>
-        <a href='https://www.linkedin.com/in/bossley9/'>
-          <FaLinkedinIn />
-          <span>linkedin.com/in/bossley9</span>
-        </a>
-        {/* TODO favicons */}
-        {/* last updated 09/30/19 */}
-        <a download target='_blank' rel='noopener noreferrer' href={`${process.env.PUBLIC_URL}/Sam_Bossley.pdf`}>
-          <FaIdBadge />
-          <span>resume</span>
-        </a>
+        <ul>
+          <li>
+            <a href='https://www.facebook.com/bossley9'>
+              <div className='link'>
+                <FaFacebookF />
+                <span>Facebook</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href='https://www.instagram.com/samaraikun/'>
+              <div className='link'>
+                <FaInstagram />
+                <span>Instagram</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href='https://soundcloud.com/sambossley'>
+              <div className='link'>
+                <FaSoundcloud />
+                <span>Soundcloud</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href='https://github.com/bossley9'>
+              <div className='link'>
+                <FaGithub />
+                <span>Github</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href='https://www.linkedin.com/in/bossley9/'>
+              <div className='link'>
+                <FaLinkedinIn />
+                <span>Linkedin</span>
+              </div>
+            </a>
+          </li>
+          <li>
+            {/* last updated 09/30/19 */}
+            <a download target='_blank' rel='noopener noreferrer' href={`${process.env.PUBLIC_URL}/Sam_Bossley.pdf`}>
+              <div className='link'>
+                <FaIdBadge />
+                <span>Resume</span>
+              </div>
+            </a>
+          </li>
+        </ul>
       </div>
     </React.Fragment>
   );
