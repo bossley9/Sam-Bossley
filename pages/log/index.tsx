4582e868e4e0b6f4262cb3e79070090cfc1e8742
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-08-01 12:12:00 -0400

    New log index

---
+++ pages/log/index.tsx
@@ -0,0 +1,79 @@
+import React, { FC, Fragment } from 'react'
+import { Head } from 'components/Head'
+import { BLink, Link } from 'components/Link'
+import { Header } from 'components/Header'
+import { getThoughtMetas } from 'util/thoughts'
+import { genFeed } from 'util/feeds'
+import { formatDate } from 'util/date'
+import { ThoughtMeta } from 'util/types'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
+import 'constants/iconLibrary'
+import { Icon } from 'components/Icon'
+import { APP_NAME, RSS_ICON_SIZE } from 'constants/strings'
+
+type Props = StaticProps & {}
+
+const meta = {
+  desc: 'my blog of opinions, reviews, and technology',
+  href: '/thoughts',
+  title: `Arbitrary Thoughts. - ${APP_NAME}`,
+}
+
+const ThoughtsContainer: FC<Props> = ({ feedUrl, thoughtMetas }) => (
+  <Fragment>
+    <Head
+      meta={{ ...meta, keywords: ['blog', 'thoughts', 'ideas', 'tutorials'] }}
+    />
+    <Header />
+    <section className="container tc mb7">
+      <h3 className="mb8">Arbitrary Thoughts.</h3>
+      <ul className="lstn mxa mb5 pl0 tl w-70-ns">
+        {thoughtMetas.map(({ id, date, title, tags }: ThoughtMeta) => {
+          return (
+            <li key={id} className="mb3">
+              <Link
+                href={`${meta.href}/[id]`}
+                as={`${meta.href}/${id}`}
+                className="db tl mb1"
+              >
+                <h5 className="ma0">{title}</h5>
+              </Link>
+              <span className="db mb1 fw700 c-text-light">
+                {formatDate(date)}
+              </span>
+              <span className="ff-libre c-text-light-2">
+                {tags.sort((a, b) => a.localeCompare(b)).join(', ')}
+              </span>
+            </li>
+          )
+        })}
+      </ul>
+      <BLink className="mb5" />
+      <div>
+        <a aria-label="rss feed" href={feedUrl} className="c-inh">
+          <Icon icon="rss" style={{ width: RSS_ICON_SIZE }} />
+        </a>
+      </div>
+    </section>
+  </Fragment>
+)
+
+export const getStaticProps: GetStaticProps = async () => {
+  const thoughtMetas = getThoughtMetas()
+  const feedUrl = genFeed({
+    ...meta,
+    items: thoughtMetas.map(({ desc, date, id, title, tags }: ThoughtMeta) => ({
+      date,
+      desc,
+      href: `${meta.href}/${id}`,
+      id,
+      tags,
+      title,
+    })),
+  })
+  return { props: { feedUrl, thoughtMetas } }
+}
+
+type StaticProps = InferGetStaticPropsType<typeof getStaticProps>
+
+export default ThoughtsContainer
